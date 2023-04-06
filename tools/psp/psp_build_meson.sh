d5e3dced1e0033e3c3c189e10ecabce716c408f9
Tobias Zindl <zie.develop@gmail.com>
2023-04-06 05:49:36 +0200

    :bricks: add Dockerfile for PSP container

--- tools/psp/psp_build_meson.sh
+++ tools/psp/psp_build_meson.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+set -e
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 ROOT_DIR="${SCRIPT_DIR}/../../"
 
@@ -8,16 +9,18 @@ BUILD_BASE_DIR="build/psp"
 function meson_build() {
     BUILD_DIR="${BUILD_BASE_DIR}/$1"
     rm -rf ${BUILD_DIR}
-    meson setup --cross-file ./tools/psp/mips_psp.build --buildtype $1 "${BUILD_DIR}"
+    meson setup --cross-file ./tools/psp/mips_psp.build \
+        --buildtype $1 "${BUILD_DIR}" \
+        -Denable_testsuite=$2
     meson compile -v -C "${BUILD_DIR}"
 }
 
 cd ${ROOT_DIR}
 
 if [ "${1}" = "Release" ]; then
-    meson_build release
+    meson_build release "false"
 elif [ "${1}" = "Size" ]; then
-    meson_build minsize
+    meson_build minsize "false"
 else
-    meson_build debug
+    meson_build debug "true"
 fi
