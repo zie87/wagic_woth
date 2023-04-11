55436df9fa16989058c7cfdd0103bdac258fea1d
Tobias Zindl <zie.develop@gmail.com>
2023-04-11 11:37:11 +0200

    :heavy_plus_sign: add unity dependency and test infrastructure

---
+++ woth/tests/unittests/src/main.cpp
@@ -0,0 +1,18 @@
+#include "unity/unity.hpp"
+
+// define JGE dummies
+int JGEGetTime() { return 0; }
+bool JGEToggleFullscreen() { return true; }
+void JGECreateDefaultBindings() {}
+
+#if defined(PSP)
+#include "JTypes.h"
+
+u8 JGEGetAnalogX() { return 0; }
+u8 JGEGetAnalogY() { return 0; }
+#endif
+
+static void RunAllTests(void) { /*RUN_TEST_GROUP(mana_base_mana);*/
+}
+
+int main(int argc, const char* argv[]) { return UnityMain(argc, argv, RunAllTests); }
